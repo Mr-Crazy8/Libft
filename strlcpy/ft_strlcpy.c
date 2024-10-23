2144f400442021b4b22232b9d700c8b2d4cc4959
Anas El mendili <anel-men@e3r8p13.1337.ma>
2024-10-23 18:20:13 +0100

    Libft01

--- strlcpy/ft_strlcpy.c
+++ strlcpy/ft_strlcpy.c
@@ -1,39 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   isalnum.c                                          :+:      :+:    :+:   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: anel-men <anel-men@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/22 13:28:53 by anel-men          #+#    #+#             */
-/*   Updated: 2024/10/22 13:33:26 by anel-men         ###   ########.fr       */
+/*   Created: 2024/10/22 13:39:55 by anel-men          #+#    #+#             */
+/*   Updated: 2024/10/22 13:55:41 by anel-men         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-#include <stdio.h>
-#include <ctype.h>
-
-int ft_isalnum(int c)
+size_t ft_strlcpy(char *dst, const char *src, size_t size)
 {
-	if (c >= '0' && c <= '9')
-	{
-		return (1);
-	}
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-	{
-		return (1);
-	}
-	else
-		return (0);
-}
+	size_t i;
 
-int main()
-{
-	char c = '*';
-	int r0 = ft_isalnum(c);
-	int r1 = isalnum(c);
-
-	printf("%d. \n", r0);
-	printf("%d. \n", r1);
+	i = 0;
+	if (size == 0)
+	{
+		return 0;
+	}
+	while (i < size - 1 && src != '\0')
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return i;
 }
